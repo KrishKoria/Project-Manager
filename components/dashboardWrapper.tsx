cf610d8cc2709bcf4dc998c14c9a7f9ed048d8ce
Krish Koria <krishkoria2004@gmail.com>
2024-10-17 23:57:12 +0530

    Add theme switch

--- components/dashboardWrapper.tsx
+++ components/dashboardWrapper.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import Navbar from "./navbar";
 import Sidebar from "./sidebar";
-import StoreProvider from "@/lib/redux";
+import StoreProvider, { useAppSelector } from "@/lib/redux";
+import { stat } from "fs";
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       <Sidebar />
       <main
-        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${isSidebarCollapsed ? "" : "md:pl-64"}`}
       >
         <Navbar />
         {children}
