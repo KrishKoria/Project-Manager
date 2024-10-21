3ac80514c14a5e0c643e4ac73d8bf3d58064895c
Krish Koria <krishkoria2004@gmail.com>
2024-10-21 22:58:28 +0530

    Refactor project routes and controller, and add taskRoutes

---
+++ server/src/controllers/taskController.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient();
+
+export const getTasks = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { projectId } = req.query;
+    const projectTasks = await prisma.task.findMany({
+      where: {
+        projectId: Number(projectId),
+      },
+      include: {
+        author: true,
+        assignee: true,
+        comments: true,
+        attachments: true,
+      },
+    });
+    res.json(projectTasks);
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ error: `Error Retrieving Tasks : ${error.message}` });
+  }
+};
