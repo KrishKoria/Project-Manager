d07b315e027b99fef1aab51cecc623b9393829f0
Krish Koria <krishkoria2004@gmail.com>
2024-10-22 19:23:50 +0530

    Refactor task routes and controller, and add createTask endpoint

--- server/src/controllers/taskController.ts
+++ server/src/controllers/taskController.ts
@@ -24,3 +24,42 @@ export const getTasks = async (req: Request, res: Response): Promise<void> => {
       .json({ error: `Error Retrieving Tasks : ${error.message}` });
   }
 };
+
+export const createTask = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const {
+    title,
+    description,
+    status,
+    priority,
+    tags,
+    startDate,
+    dueDate,
+    points,
+    projectId,
+    authorUserId,
+    assignedUserId,
+  } = req.body;
+  try {
+    const newTask = await prisma.task.create({
+      data: {
+        title,
+        description,
+        status,
+        priority,
+        tags,
+        startDate,
+        dueDate,
+        points,
+        projectId,
+        authorUserId,
+        assignedUserId,
+      },
+    });
+    res.status(201).json(newTask);
+  } catch (error: any) {
+    res.status(500).json({ error: `Error Creating Task: ${error.message}` });
+  }
+};
