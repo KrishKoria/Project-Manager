52a956c1b8549458ea41d55af821356e48a36747
Krish Koria <krishkoria2004@gmail.com>
2024-10-25 11:21:26 +0530

    Added Tab Buttons

--- client/components/ProjectHeader.tsx
+++ client/components/ProjectHeader.tsx
@@ -1,9 +1,34 @@
 import React, { useState } from "react";
 import Header from "./Header";
+import { Clock, Grid3X3, List, Table } from "lucide-react";
 
 type Props = {
   activeTab: string;
   setActiveTab: (tab: string) => void;
+};
+
+type TabButtonProps = {
+  icon: React.ReactNode;
+  name: string;
+  setActiveTab: (tab: string) => void;
+  activeTab: string;
+};
+
+const TabButton = ({ name, icon, setActiveTab, activeTab }: TabButtonProps) => {
+  const isActive = activeTab === name;
+  return (
+    <button
+      onClick={() => setActiveTab(name)}
+      className={`flex cursor-pointer items-center gap-2 rounded-md px-1 py-2 ${
+        isActive
+          ? "text-blue-600 after:bg-blue-600 dark:text-white"
+          : "text-neutral-500 dark:text-neutral-400"
+      } after:absolute after:-bottom-[-9px] after:left-0 after:h-1 after:w-full hover:text-blue-600 dark:text-neutral-500 dark:hover:text-white lg:px-4`}
+    >
+      {icon}
+      <span>{name}</span>
+    </button>
+  );
 };
 
 const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
@@ -13,6 +38,35 @@ const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
       {/* Modal */}
       <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
         <Header name="Product Design Development" />
+      </div>
+      {/* Tabs */}
+      <div className="flex flex-wrap-reverse gap-2 border-y border-gray-200 pb-2 pt-2 dark:border-stroke-dark md:items-center">
+        <div className="flex flex-1 items-center gap-2 md:gap-4">
+          <TabButton
+            name="Board"
+            icon={<Grid3X3 className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+          <TabButton
+            name="List"
+            icon={<List className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+          <TabButton
+            name="Timeline"
+            icon={<Clock className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+          <TabButton
+            name="Table"
+            icon={<Table className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+        </div>
       </div>
     </div>
   );
